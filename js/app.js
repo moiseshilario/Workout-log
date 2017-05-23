37c838fced16ed5422b6d592ac42d73c7a12dee0
moiseshilario <moiseshilariu@hotmail.com>
2017-05-23 17:19:44 -0300

    Refactor app.js to "Module Pattern"

--- js/app.js
+++ js/app.js
@@ -1,36 +1,46 @@
-let workouts = [];
-let sum = 0;
+let app = function () {
 
-function addActivity() {
-    const FORM_SIZE = 3;
-    let form = document.getElementById("frm");
-    let inputs = [];
-    
-    for (let index = 0; index < FORM_SIZE; index++) {
-        inputs.push(form.elements[index].value);
+    let workouts = [];
+    let sum = 0;
+
+    function addActivity(){
+        const FORM_SIZE = 3;
+        let form = document.getElementById("frm");
+        let inputs = [];
+
+        for (let index = 0; index < FORM_SIZE; index++) {
+            inputs.push(form.elements[index].value);
+        }
+
+        let workout = new Workout(...inputs);
+        addWorkoutInTable(workout);
+        sumHours(parseInt(workout.time));
+        workouts.push(workout);
+        workout.showWorkout();
     }
 
-    let workout = new Workout(...inputs);
-    addWorkoutInTable(workout);
-    sumHours(parseInt(workout.time));
-    workouts.push(workout);
-    workout.showWorkout();
-}
+    function addWorkoutInTable(workout) {
+        let table = document.getElementById("workoutTable");
+        let row = table.insertRow(1);
+        let cell = [];
+        let btnDelete = document.createElement("button");
+        let t = document.createTextNode("-");
+        btnDelete.appendChild(t);
 
-function addWorkoutInTable(workout) {
-    let table = document.getElementById("workoutTable");
-    let row = table.insertRow(1);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
+        for (let i = 0; i < 4; i++) {
+            cell.push(row.insertCell(i));
+        }
 
-    cell1.innerHTML = `${workout.time} h`;
-    cell2.innerHTML = workout.activity;
-    cell3.innerHTML = workout.date;
-}
+        cell[0].innerHTML = `${workout.time} h`;
+        cell[1].innerHTML = workout.activity;
+        cell[2].innerHTML = workout.date;
+        cell[3].appendChild(btnDelete);
+    }
 
-function sumHours(hours) {
-    sum += hours;
-    document.getElementById("sum").innerHTML = `${sum} hours of exercise`;
-}
+    function sumHours(hours) {
+        sum += hours;
+        document.getElementById("sum").innerHTML = `${sum} hours of exercise`;
+    }
 
+    return { addActivity: addActivity, addWorkoutInTable: addWorkoutInTable, sumHours: sumHours };
+}();
--- js/workout.js
+++ js/workout.js
@@ -1,11 +1,11 @@
 class Workout {
-    constructor(time, activity, date){
+    constructor(time, activity, date) {
         this.time = time;
         this.activity = activity;
         this.date = date;
     }
 
-    showWorkout(){
+    showWorkout() {
         console.log(`Time spent ${this.time}
 Activity name: ${this.activity}
 Date: ${this.date}`);
