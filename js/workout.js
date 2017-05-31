c1279d415f8d0d5e3f3799a2852f61040c3b9a85
moiseshilario <moiseshilariu@hotmail.com>
2017-05-31 11:45:04 -0300

    Format date in Workout class

--- js/workout.js
+++ js/workout.js
@@ -1,7 +1,14 @@
 class Workout {
     constructor(time, activity, date) {
+        const formatDate = date.replace(/\-/g, '/');
+        const dateOptions = {
+           day: "2-digit",
+           month: "2-digit",
+           year: "numeric"
+        };
+
         this.time = time;
         this.activity = activity;
-        this.date = date;
+        this.date = new Date(formatDate).toLocaleDateString('en-US', dateOptions);
     }
 }
